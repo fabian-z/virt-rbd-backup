51eb7d3fb96d67d7e6e4cd3331f306300064e543
Fabian Zaremba <f.zaremba@konrad-technologies.de>
2021-05-25 02:08:06 +0200

    Change copy logic (RBD is not file-like)

--- output/borg.py
+++ output/borg.py
@@ -3,7 +3,10 @@ from datetime import datetime
 import shutil
 
 BACKUP_BASE = "rbd_backup"
+CHUNK_SIZE = 16384 # 16kb chunks
 
+class BackupException(Exception):
+    pass
 
 def backup(target_repo, src, filename=None, progress=False):
     timestamp = datetime.utcnow().strftime('%Y_%m_%d_%s')
@@ -11,7 +14,27 @@ def backup(target_repo, src, filename=None, progress=False):
     process = subprocess.Popen(("/usr/bin/borg", "create", "-p", target, "-"),
                                stdin=subprocess.PIPE, stdout=subprocess.DEVNULL)
 
-    shutil.copyfileobj(src, process.stdin)
+    size = src.size()
+
+    offset = 0
+    write_last = False
+    while:
+        if offset + CHUNK_SIZE > size:
+            last_chunk = size - offset
+            data = src.read(offset, last_chunk)
+            write_last = True
+        else
+            data = src.read(offset, CHUNK_SIZE)
+
+        offset += len(data)
+        written = process.stdin.write(data)
+
+        if written != len(data)
+            raise BackupException("Short write")
+
+        if write_last:
+            break
+    
     process.stdin.close()
 
     return_code = process.wait()
